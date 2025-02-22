fe29fc0ac70a9ca8ee99bb6f28152c4ce8838503
Fernando Bade <bade.fernando@gmail.com>
2025-02-22 16:30:04 -0300

    ajuste deploy

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,19 @@ echo "Iniciando o deploy da aplicação..."
 
 cd /home/u657600000/domains/bade.digital/public_html/laurus-api
 
+# Ativar o NVM antes de executar comandos do Node.js
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm use 20.15.1
+
 # Instalar dependências
 npm install
 
-# Rodar as migrations do Prisma (caso esteja usando)
-npx prisma migrate deploy
-npx prisma generate
+# Rodar Prisma especificando o caminho correto do schema
+npx prisma migrate deploy --schema=src/prisma/schema.prisma
+npx prisma generate --schema=src/prisma/schema.prisma
 
-# Construir a aplicação (caso use TypeScript ou outro build)
+# Build do projeto
 npm run build
 
 echo "Deploy concluído com sucesso!"
